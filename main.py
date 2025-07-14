5f9c02acb0ebaa23d30cb5ce15c2c9e6baffbbd3
Vinay Kumar <vinayjangra.dev@gmail.com>
2025-07-14 09:37:45 +0530

    Setup UI

---
+++ main.py
@@ -0,0 +1,46 @@
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+from tkinter import *
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+title_label.grid(column=1, row=0)
+
+start_button = Button(text="Start")
+start_button.grid(row=2, column=0)
+
+resest_button = Button(text="Reset")
+resest_button.grid(row=2, column=2)
+
+checkbox = Checkbutton(text="✅")
+checkbox.grid(column=1, row=3)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+
+
+window.mainloop()
