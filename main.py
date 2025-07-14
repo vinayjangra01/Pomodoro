fc211660de5256d34d73389d510425268f1b031c
Vinay Kumar <vinayjangra.dev@gmail.com>
2025-07-14 10:05:31 +0530

    Implement timer mechanism

--- main.py
+++ main.py
@@ -11,34 +11,41 @@ LONG_BREAK_MIN = 20
 
 from tkinter import *
 
-# ---------------------------- TIMER RESET ------------------------------- # 
-
-
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
-# ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(25)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if(count > 0):
+        window.after(1000, count_down, count - 1)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
 title_label.grid(column=1, row=0)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
 resest_button = Button(text="Reset", highlightthickness=0)
 resest_button.grid(row=2, column=2)
 
-
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(row=1, column=1)
 
 
 check_marks = Label(text="✔️", fg=GREEN, bg=YELLOW)
