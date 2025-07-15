70f744b9d20fe226317e878b04885da6da2b1e3a
Vinay Kumar <vinayjangra.dev@gmail.com>
2025-07-15 10:06:33 +0530

    Add break and focus timer

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 1
+SHORT_BREAK_MIN = 0.5
+LONG_BREAK_MIN = 1
+reps = 0
 
 from tkinter import *
 
@@ -27,12 +28,43 @@ canvas.grid(row=1, column=1)
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(25)
+    global reps
+    reps  = reps + 1
+    work_sec = WORK_MIN * 60
+    short_break_secs = SHORT_BREAK_MIN * 60
+    long_break_secs = LONG_BREAK_MIN * 60
+
+    #1, 3, 5, 7 => 25 minutes
+    #2,4,6, => 5 minutes break
+    #8th => long break
+
+    if reps % 8 == 0:
+        title_label.config(text="Long Break", fg=RED)
+        count_down(long_break_secs)
+    elif reps % 2 == 1:
+        title_label.config(text="Focus", fg=GREEN)
+        count_down(work_sec)
+    else:
+        title_label.config(text="Short Break", fg=PINK)
+        count_down(short_break_secs)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+
+    minutes = math.floor(count / 60)
+    seconds = count % 60
+
+    if minutes <= 9:
+        minutes = f"0{minutes}"
+    
+    if seconds <= 9:
+        seconds = f"0{seconds}"
+
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
     if(count > 0):
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
